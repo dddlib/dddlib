11318963330c6ecee1001067adc0adc7ac93982a
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2014-06-13 07:47:34 +0200

    Added identity map interface placeholder

---
+++ src/dddlib.Persistence/IIdentityMap.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="IIdentityMap.cs" company="dddlib contributors">
+//  Copyright (c) dddlib contributors. All rights reserved.
+// </copyright>
+
+namespace dddlib.Persistence
+{
+    /// <summary>
+    /// Exposes the public members of the identity map.
+    /// </summary>
+    public interface IIdentityMap
+    {
+    }
+}
