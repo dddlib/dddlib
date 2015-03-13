e7c79013ebdc2841d7be0f9b7250a2353d19682f
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2015-03-13 16:29:00 +0100

    Refactored entity type

--- src/dddlib/Sdk/Configuration/Model/EntityType.cs
+++ src/dddlib/Sdk/Configuration/Model/EntityType.cs
@@ -9,10 +9,18 @@ namespace dddlib.Sdk.Configuration.Model
     using System.Globalization;
     using dddlib.Sdk.Configuration.Model.TypeAnalyzerService;
 
-    internal class EntityType : Entity
+    /// <summary>
+    /// Represents an entity type.
+    /// </summary>
+    public class EntityType : Entity
     {
         private static readonly ITypeAnalyzerService DefaultTypeAnalyzerService = new DefaultTypeAnalyzerService();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityType"/> class.
+        /// </summary>
+        /// <param name="runtimeType">Type of the runtime.</param>
+        /// <param name="typeAnalyzerService">The type analyzer service.</param>
         public EntityType(Type runtimeType, ITypeAnalyzerService typeAnalyzerService)
              : base(new NaturalKey(typeof(EntityType), "RuntimeType", typeof(Type), DefaultTypeAnalyzerService), EqualityComparer<object>.Default)
         {
@@ -31,6 +39,12 @@ namespace dddlib.Sdk.Configuration.Model
             this.Mappings = new MapperCollection();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EntityType"/> class.
+        /// </summary>
+        /// <param name="runtimeType">Type of the runtime.</param>
+        /// <param name="entityAnalyzerService">The entity analyzer service.</param>
+        /// <param name="baseEntity">The base entity.</param>
         public EntityType(Type runtimeType, ITypeAnalyzerService entityAnalyzerService, EntityType baseEntity)
             : this(runtimeType, entityAnalyzerService)
         {
@@ -50,16 +64,36 @@ namespace dddlib.Sdk.Configuration.Model
             this.NaturalKey = this.NaturalKey ?? baseEntity.NaturalKey;
         }
 
+        /// <summary>
+        /// Gets the runtime type for this entity type.
+        /// </summary>
+        /// <value>The runtime type.</value>
         [dddlib.NaturalKey]
         public Type RuntimeType { get; private set; }
 
+        /// <summary>
+        /// Gets the natural key for this entity type.
+        /// </summary>
+        /// <value>The natural key.</value>
         public NaturalKey NaturalKey { get; private set; }
 
+        /// <summary>
+        /// Gets the natural key equality comparer for this entity type.
+        /// </summary>
+        /// <value>The natural key equality comparer.</value>
         public IEqualityComparer<object> NaturalKeyEqualityComparer { get; private set; }
 
-        // TODO (Cameron): This is not right. Law of Dementer and all that.
+        /// <summary>
+        /// Gets the mappings.
+        /// </summary>
+        /// <value>The mappings.</value>
+        //// TODO (Cameron): This is not right. Law of Dementer and all that.
         public MapperCollection Mappings { get; private set; }
 
+        /// <summary>
+        /// Configures the natural key for this entity type.
+        /// </summary>
+        /// <param name="naturalKey">The natural key.</param>
         public void ConfigureNaturalKey(NaturalKey naturalKey)
         {
             Guard.Against.Null(() => naturalKey);
@@ -92,6 +126,11 @@ namespace dddlib.Sdk.Configuration.Model
             this.NaturalKey = naturalKey;
         }
 
+        /// <summary>
+        /// Configures the natural key for this entity type.
+        /// </summary>
+        /// <param name="naturalKey">The natural key.</param>
+        /// <param name="naturalKeyEqualityComparer">The natural key equality comparer.</param>
         public void ConfigureNaturalKey(NaturalKey naturalKey, IEqualityComparer<string> naturalKeyEqualityComparer)
         {
             Guard.Against.Null(() => naturalKey);
