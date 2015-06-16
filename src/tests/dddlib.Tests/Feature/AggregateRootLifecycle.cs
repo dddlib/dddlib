2743d7f7739044be1418f09d40c9db729eab6a1b
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2015-06-16 20:08:43 +0200

    Updated lifecycle tests

--- src/tests/dddlib.Tests/Feature/AggregateRootLifecycle.cs
+++ src/tests/dddlib.Tests/Feature/AggregateRootLifecycle.cs
@@ -14,53 +14,103 @@ namespace dddlib.Tests.Feature
     // I need to be able to end the lifecycle of an entity
     public class AggregateRootLifecycle : Feature
     {
-        /*  TODO (Cameron):
-            Needs to support PODO (plain old domain objects)  */
-
-        [Scenario]
-        public void CanDestroy(Action action)
+        public class DefaultLifecycle : AggregateRootLifecycle
         {
-            var subject = default(Subject);
+            [Scenario]
+            public void Scenario(Action action)
+            {
+                var subject = default(Subject);
 
-            "Given a subject"
-                .f(() => subject = new Subject());
+                "Given a subject"
+                    .f(() => subject = new Subject());
 
-            "And the subject is updated"
-                .f(() => subject.Update());
+                "And the subject is updated"
+                    .f(() => subject.Update());
 
-            "And the subject is destroyed"
-                .f(() => subject.Destroy());
+                "And the subject is destroyed"
+                    .f(() => subject.Destroy());
 
-            "When the subject is updated again"
-                .f(() => action = () => subject.Update());
+                "When the subject is updated again"
+                    .f(() => action = () => subject.Update());
 
-            "Then that action should throw an exception"
-                .f(() => action.ShouldThrow<dddlib.BusinessException>());
+                "Then that action should throw an exception"
+                    .f(() => action.ShouldThrow<dddlib.BusinessException>());
+            }
+
+            private class Subject : AggregateRoot
+            {
+                private int version;
+
+                public void Update()
+                {
+                    // NOTE (Cameron): We have to do the heavy lifting ourselves in this instance.
+                    if (this.IsDestroyed)
+                    {
+                        throw new dddlib.BusinessException("Lifecycle has ended!");
+                    }
+
+                    this.version = this.version + 1;
+                }
+
+                public void Destroy()
+                {
+                    this.EndLifecycle();
+                }
+            }
+
+            private class SubjectUpdated
+            {
+                public int Version { get; set; }
+            }
         }
 
-        private class Subject : AggregateRoot
+        public class EventBasedLifecycle : AggregateRootLifecycle
         {
-            private int version;
-
-            public void Update()
+            [Scenario]
+            public void Scenario(Action action)
             {
-                this.Apply(new SubjectUpdated { Version = this.version + 1 });
+                var subject = default(Subject);
+
+                "Given a subject"
+                    .f(() => subject = new Subject());
+
+                "And the subject is updated"
+                    .f(() => subject.Update());
+
+                "And the subject is destroyed"
+                    .f(() => subject.Destroy());
+
+                "When the subject is updated again"
+                    .f(() => action = () => subject.Update());
+
+                "Then that action should throw an exception"
+                    .f(() => action.ShouldThrow<dddlib.BusinessException>());
             }
 
-            public void Destroy()
+            private class Subject : AggregateRoot
             {
-                this.EndLifecycle();
+                private int version;
+
+                public void Update()
+                {
+                    this.Apply(new SubjectUpdated { Version = this.version + 1 });
+                }
+
+                public void Destroy()
+                {
+                    this.EndLifecycle();
+                }
+
+                private void Handle(SubjectUpdated @event)
+                {
+                    this.version = @event.Version;
+                }
             }
 
-            private void Handle(SubjectUpdated @event)
+            private class SubjectUpdated
             {
-                this.version = @event.Version;
+                public int Version { get; set; }
             }
-        }
-
-        private class SubjectUpdated
-        {
-            public int Version { get; set; }
         }
     }
 }
